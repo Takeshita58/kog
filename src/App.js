de2492f97609dd92dabfa85701820889b021ed74
H T <h.takeshita@varnux.com>
2023-01-13 14:09:06 +0900

    del tailwind

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="bg-red-500 text-center">
+    <div className="App">
       <header className="App-header">
         $SHIBI
       </header>
