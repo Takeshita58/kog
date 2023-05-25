a5767d3ae2d088d6b59a1f744629677fe89c3b7a
H T <h.takeshita@varnux.com>
2023-05-25 14:05:45 +0900

    css16

--- src/App.js
+++ src/App.js
@@ -277,10 +277,12 @@ function App() {
                       genImage()
               }}>Mint</button>
             ):(
-              <button  style={{color:"white", backgroundColor:"#808080", borderRadius:"2px", paddingRight:"4px", paddingLeft:"3px", display:"block"}} 
-              onClick={async(event)=>{
-                metamaskLogin(event)
-              }}>Connect</button>
+              <div style={{textAlign:"center"}}>
+                <button  style={{color:"white", backgroundColor:"#808080", borderRadius:"2px", paddingRight:"4px", paddingLeft:"3px"}} 
+                onClick={async(event)=>{
+                  metamaskLogin(event)
+                }}>Connect</button>
+              </div>
             )} 
             <canvas style={{margin:"auto", marginTop:"10px"}} width="150" height="150" id="canvas"></canvas>
             <button style={{color:"white", backgroundColor:"#808080", borderRadius:"2px", marginBottom:"10px",paddingRight:"4px", paddingLeft:"3px", display:"block", margin:"auto"}} 
